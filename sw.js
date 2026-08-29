42d7dd73000108a76eba39586a9db2ba0f1cbd80
Aprilmax <taohua25@outlook.com>
2026-08-29 22:57:44 +0800

    Bump cache for document export update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kaoyan-review-v24';
+const CACHE='kaoyan-review-v25';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(async c=>{for(const url of ASSETS){try{await c.add(new Request(url,{cache:'reload'}))}catch(_){}}}).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
